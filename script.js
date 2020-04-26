602fc374a6a7521d96524ca1675dbc88f0cd1fb4
Saular Moaddeli <jake.moaddeli@gmail.com>
2020-04-26 09:00:11 -0700

    building rows in js

--- script.js
+++ script.js
@@ -14,7 +14,46 @@ $(document).ready(function() {
         eventArr = new Array(9);
     }
 
-    
+    // Grab container div and reset
+    var planner = $("#plannerContainer");
+    planner.empty();
+
+    // Build day calendar
+    for (var hr = 9; hr = 17; hr++) {
+        var arrIndex = hr - 9;
+
+        // Build row divs
+        var rowDiv = $("<div>");
+        rowDiv.addClass("time-block");
+        rowDiv.addClass("row");
+        rowDiv.attr("hour", hr);
+
+        // Build time box
+        var timeBoxDiv = $("<div>");
+        timeBoxDiv.addClass("col-2");
+
+        var timeBoxSpan = $("<span>");
+        timeBoxSpan.addClass("hour");
+
+        // Format time
+        var timeDisplay = 0;
+        var amOrPm = "";
+
+        if (hr > 12) {
+            timeDisplay = hr - 12;
+            amOrPm = "pm";
+        }
+        else {
+            timeDisplay = hr;
+            amOrPm = "am";
+        }
+
+        timeBoxSpan.text(timeDisplay + amOrPm);
+        rowDiv.append(timeBoxDiv);
+        timeBoxDiv.append(timeBoxSpan);
+
+
+    }
 
 
 
